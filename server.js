66e6fa7ee149ad923854843550eb5180034bcae9
dreamPerson <111998230+dreamPerson0106@users.noreply.github.com>
2023-10-13 06:03:43 +0900

    port update

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-server.listen(8080, "0.0.0.0", () => {
+server.listen(80, "0.0.0.0", () => {
   console.log("Listening to port 8080");
 });
