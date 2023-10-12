a4232c62879a1d7e0689bddb75f35d73cab881c8
dreamPerson <111998230+dreamPerson0106@users.noreply.github.com>
2023-10-13 05:55:43 +0900

    first

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const WebSocket = require("ws");
+
+const server = http.createServer(app);
+
+const WebSocketServer = new WebSocket.Server({ server });
+
+WebSocketServer.on("connection", (ws) => {
+  ws.on("message", function incoming(message, isBinary) {
+    console.log(message.toString(), isBinary);
+    WebSocketServer.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+server.listen(8080, "0.0.0.0", () => {
+  console.log("Listening to port 8080");
+});
