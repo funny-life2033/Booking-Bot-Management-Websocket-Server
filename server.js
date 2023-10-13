264b636beb9b63f7e9e67328a7d75fcd43657aea
dreamPerson <111998230+dreamPerson0106@users.noreply.github.com>
2023-10-13 15:51:20 +0900

    bot complete

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ const server = http.createServer(app);
 
 const WebSocketServer = new WebSocket.Server({ server });
 
-let webSocketClients = [];
+let botClients = [];
 WebSocketServer.on("connection", (ws) => {
   ws.on("message", function incoming(message, isBinary) {
     const msg = message.toString();
-    console.log(msg, isBinary);
     // WebSocketServer.clients.forEach(function each(client) {
     //   if (client.readyState === WebSocket.OPEN) {
     //     client.send(message.toString());
@@ -19,15 +18,51 @@ WebSocketServer.on("connection", (ws) => {
     // });
     const msgStr = msg.split("-");
     if (msgStr[0] === "bot") {
-      webSocketClients.push({ ws, id: msgStr[1] });
+      botClients.push({ ws, id: msgStr[1], requestingApps: [] });
+      console.log("increased to: ", botClients);
       ws.send("connected to server");
+    } else if (msgStr[0] === "app") {
+      let bot = botClients.find((client) => client.id === msgStr[1]);
+      if (bot) {
+        const app = bot.requestingApps.find((app) => app.id === msgStr[1]);
+        if (app) {
+          ws.send("failed-You have already requested");
+        } else {
+          bot.requestingApps.push({ ws, id: msgStr[1] });
+          botClients = botClients.map((client) => {
+            if (client.id === msgStr[1]) {
+              client.ws.send(`connecting-${msgStr[1]}`);
+              return bot;
+            } else return client;
+          });
+        }
+      } else {
+        ws.send("failed-This device doesn't exist");
+      }
     }
   });
 
-  ws.on("close", () => {
-    webSocketClients = webSocketClients.filter((client) => client.ws !== ws);
-    console.log(webSocketClients);
-  });
+  const closeHandle = () => {
+    let newBotClients = [];
+    botClients.forEach((client) => {
+      if (client.ws !== ws) {
+        client.requestingApps = client.requestingApps.filter((app) => {
+          if (app.ws === ws) {
+            client.ws.send(`disconnecting-${app.id}`);
+            return false;
+          }
+          return true;
+        });
+        newBotClients.push(client);
+      }
+    });
+    botClients = newBotClients;
+    console.log("reduced to: ", botClients);
+  };
+
+  ws.on("close", closeHandle);
+
+  ws.on("error", closeHandle);
 });
 
 app.get("/", (req, res) => {
