83f5dadfc5b86a5d6a967ef8e652f1d908a86f49
dreamPerson <111998230+dreamPerson0106@users.noreply.github.com>
2023-10-27 20:58:14 +0900

    bug fix

--- controllers/adiClient.js
+++ controllers/adiClient.js
@@ -19,7 +19,7 @@ const login = async (req, res) => {
       return res.status(400).json({ error: "This username doesn't exist" });
     }
 
-    const auth = await bcrypt.compare(password, user.password);
+    const auth = await bcrypt.compare(password, client.password);
     if (!auth) {
       return res.status(400).json({ error: "Incorrect password" });
     }
